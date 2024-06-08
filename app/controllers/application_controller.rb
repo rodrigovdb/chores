9d4233d9fce04693c4c13a4dd45645c34b3c4aed
Rodrigo Otavio van den Berg Maia <rodrigo.maia@wekowtraining.ca>
2024-06-08 08:48:47 -0400

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def index
+    @chores = Chore.all
+    @daily_chores = DailyChore.for_this_week
+    @week = Date.current.beginning_of_week..Date.current.end_of_week
+  end
 end
--- app/models/daily_chore.rb
+++ app/models/daily_chore.rb
@@ -2,4 +2,7 @@ class DailyChore < ApplicationRecord
   belongs_to :chore
 
   validates :chore, presence: true
+
+  scope :for_this_week, -> { where(created_at: Date.current.beginning_of_week..Date.current.end_of_week) }
+  scope :for_today, -> { where(created_at: Date.current.all_day) }
 end
