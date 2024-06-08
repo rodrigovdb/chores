dc85c61426b002b57cf6f9b6c2c29321e47e55d6
Rodrigo Otavio van den Berg Maia <rodrigo.maia@wekowtraining.ca>
2024-06-08 09:08:32 -0400

    week table

--- app/models/daily_chore.rb
+++ app/models/daily_chore.rb
@@ -4,5 +4,7 @@ class DailyChore < ApplicationRecord
   validates :chore, presence: true
 
   scope :for_this_week, -> { where(created_at: Date.current.beginning_of_week..Date.current.end_of_week) }
-  scope :for_today, -> { where(created_at: Date.current.all_day) }
+  scope :for_day, ->(date) { where(created_at: date.all_day) }
+  scope :for_chore, ->(chore) { where(chore:) }
+  scope :for_today, -> { for_day(Date.current) }
 end
