4a2a960cf71f6d113e2e0168f92fe76b162324e5
Rodrigo Otavio van den Berg Maia <rodrigo.maia@wekowtraining.ca>
2024-07-06 00:01:11 -0400

    flatpickr

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
+import Flatpickr from 'stimulus-flatpickr'
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+application.register('flatpickr', Flatpickr)
 
 export { application }
