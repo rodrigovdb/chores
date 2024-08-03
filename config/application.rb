eccc36331d0c3b40e9d96baa2fb32f1e2c748302
Rodrigo Otavio van den Berg Maia <rodrigo.maia@wekowtraining.ca>
2024-08-02 20:58:00 -0400

    hsts

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Chores
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts << 'chores'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
