19b8a74338e0c0b3d011dedf5242e43c0c0cdfa5
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2024-12-31 19:04:20 -0500

    add kid to chore

--- db/migrate/20241231141454_add_kid_to_daily_chore.rb
+++ db/migrate/20241231141454_add_kid_to_daily_chore.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddKidToDailyChore < ActiveRecord::Migration[7.0]
   def change
     add_reference :daily_chores, :kid, null: true, foreign_key: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_31_030744) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_31_141454) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_31_030744) do
     t.bigint "chore_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "kid_id"
     t.index ["chore_id"], name: "index_daily_chores_on_chore_id"
+    t.index ["kid_id"], name: "index_daily_chores_on_kid_id"
   end
 
   create_table "kid_chores", force: :cascade do |t|
@@ -64,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_31_030744) do
 
   add_foreign_key "chores", "users"
   add_foreign_key "daily_chores", "chores"
+  add_foreign_key "daily_chores", "kids"
   add_foreign_key "kid_chores", "chores"
   add_foreign_key "kid_chores", "kids"
   add_foreign_key "kids", "users"
