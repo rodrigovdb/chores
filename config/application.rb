36521fa51b2783add6e67a32e8eefe47400cb773
Rodrigo Otavio van den Berg Maia <rodrigovdb@gmail.com>
2025-01-05 21:11:59 -0500

    application timezone

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,8 @@ module Chores
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+
+    config.time_zone = "Eastern Time (US & Canada)"
     config.eager_load_paths << Rails.root.join('lib')
   end
 end
